be9c96c713da98da72dfe3b1aaaff0a7f25e8be2
pitronalldak <ryabinin.p.b@gmail.com>
2020-08-02 16:39:56 +0300

    Fix deploy

---
+++ build/precache-manifest.4a3fe933ad1b0591fb4e8ed39dfbda81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dced6c4b4360175a1e4d2033158f5742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29cc43a3f6f0bcc9914f",
+    "url": "/static/js/2.cf9721f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cf9721f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a554b9a25f549c3e983f",
+    "url": "/static/js/main.250d7699.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/static/media/blog-post-1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/static/media/blog-post-2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/static/media/blog-post-3.8d6e276b.md"
+  }
+]);
